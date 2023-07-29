1dc8a1080bf094d40be1d83e0aa93263b12ec8e4
Steven Gutierrez <stevogbmv@gmail.com>
2023-07-28 17:20:57 -0700

    commit tree 3f1035a6c6d756b314f0743181ef4ad10d0cfaba

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :sufficiently_clickbait_y
+
+    CLICKBAIT_OPTIONS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def sufficiently_clickbait_y
+        if CLICKBAIT_OPTIONS.none? { |opt| opt.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
